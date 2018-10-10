a9ad528a7e937ca22f6e4e4e83bef62baf34fd6d
Vincent <445052471@qq.com>
2018-10-10 17:03:14 +0800

    完善http rpc,完善泛型结果集返回处理

--- src/main/java/com/lanhun/example/service/impl/HelloServiceImpl.java
+++ src/main/java/com/lanhun/example/service/impl/HelloServiceImpl.java
@@ -3,6 +3,7 @@ package com.lanhun.example.service.impl;
 
 import com.lanhun.example.RemoteExampleService;
 import com.lanhun.example.service.HelloService;
+import com.lanhun.system.JsonMapper;
 import com.lanhun.system.RemoteClient;
 import com.lanhun.system.model.Branch;
 import java.util.HashMap;
@@ -24,7 +25,7 @@ public class HelloServiceImpl implements HelloService {
 */
         param.put("page_no",1);
         param.put("page_size",10);
-       List<Branch> remoteResult= remoteExampleService.invoke(param);
-        return name +" Hello ,remote result:"+remoteResult;
+        List<Branch> remoteResult= remoteExampleService.invoke(param);
+        return name +" Hello ,Remote Result:"+JsonMapper.toJsonString(remoteResult);
     }
 }
